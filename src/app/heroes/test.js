e14193a717799121ac2f651642dab4fed113f398
Hiumathy <hiumathy@Hiumathys-MacBook-Pro.local>
2018-07-10 05:38:52 -0400

    the hero editor

---
+++ src/app/heroes/test.js
@@ -0,0 +1,33 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.move = function (distance) {
+        if (distance === void 0) { distance = 0; }
+        console.log(this.name + " moved " + distance);
+    };
+    return Animal;
+}());
+var Snake = /** @class */ (function (_super) {
+    __extends(Snake, _super);
+    function Snake(name) {
+        return _super.call(this, name) || this;
+    }
+    Snake.prototype.move = function (distance) {
+        if (distance === void 0) { distance = 5; }
+        _super.prototype.move.call(this, distance);
+    };
+    return Snake;
+}(Animal));
+var tested = new Animal('Bug').move();
+var snake = new Snake('Snake').move();
